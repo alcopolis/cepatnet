4e273c7e29a0c9cf7732cd5c644e3c4dc50744ab
alcopolis <myseconddigitalmail@yahoo.com>
2014-01-30 17:25:17 -0800

    30 Jan 2014

--- addons/shared_addons/themes/cepatnet/views/modules/epg/tv-guide.php
+++ addons/shared_addons/themes/cepatnet/views/modules/epg/tv-guide.php
@@ -1,19 +1,40 @@
 <div style="margin:0 auto; width:960px;">
-	<img src="http://www.cepat.net.id/tv/images/tvcable.jpg">
+	<img src="{{theme:image_path}}tv-guide.png" style="width:359px;">
     
-    <div style="width:100%; height:500px; background:#59A; margin:20px 0;">
-    	<div class="sch-util">
-	    	<p class="today"><?php echo date('d F Y'); ?></p>
-	        <div class="sch-input">
-	            <form method="get" action="/tv/index.php">
-	            	<?php echo form_dropdown('ch', $ch, set_value('ch', '1')); ?>
-	            	
-	                <input type="text" value="29-01-2014" id="date" name="date" class="hasDatepicker"><img class="ui-datepicker-trigger" src="images/calendar.png" alt="..." title="...">
-	                <input type="submit" value="view">
-	            </form>
-	        </div>
-	        <div style="clear:both;"></div>
-	    </div>
+    <div class="sch-util" style="width:100%; background:#59A; margin:20px 0;">
+    	<p class="today"><?php echo date('d F Y'); ?></p>
+        <div class="sch-input">
+            <form method="get" action="/tv/index.php">
+            	<?php echo form_dropdown('ch', $ch, set_value('ch', '1')); ?>
+            	
+                <input type="text" value="29-01-2014" id="date" name="date" class="hasDatepicker"><img class="ui-datepicker-trigger" src="images/calendar.png" alt="..." title="...">
+                <input type="submit" value="view">
+            </form>
+        </div>
+        <div style="clear:both;"></div>
+    </div>
+    
+    <div style="width:100%; background:#59A; margin:20px 0;">
+	    <?php if($shows){ ?>
+	    	<?php foreach($shows as $show){ ?>
+	    		<div style="margin:10px 0; background:#FFF; padding:20px;">
+	    			<h3><?php echo $show->ch->name; ?></h3>
+	    			<ul>
+	    		<?php if(!empty($show->sh)){ foreach($show->sh as $s){ ?>
+	    				<li>
+	    					<?php echo $s->time . ' | '; ?>
+	    					<?php echo $s->duration . '<br/>'; ?>
+	    					<?php echo $s->title . '<br/>'; ?>
+	    					<?php echo $s->syn_id . '<br/>'; ?>
+	    					<?php echo $s->syn_en . '<br/>'; ?>
+	    				</li>
+	    		<?php }}else{ echo 'No EPG Data'; } ?>
+	    			</ul>
+	    		</div>
+	    	<?php } ?>
+	    <?php }else{ ?>
+	    	tv guide home
+	    <?php } ?>	
     </div>
     
     <img src="http://www.cepat.net.id/tv/images/tvcable2.jpg">
